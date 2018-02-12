8e1cabbede29b309945142d8bd7e2082961874e9
Huhtilainen Heli M <hdheli@lx7-fuxi083.ad.helsinki.fi>
2018-02-12 23:31:52 +0200

    kategoria-luokan täydennystä

--- src/main/java/reseptish/pojo/Kategoria.java
+++ src/main/java/reseptish/pojo/Kategoria.java
@@ -5,10 +5,67 @@
  */
 package reseptish.pojo;
 
+import java.util.Objects;
+
 /**
  *
  * @author jaakko
  */
 public class Kategoria {
+
+    private Integer kategoriaId;
+    private String nimi;
+
+    public Kategoria(Integer kategoriaId, String nimi) {
+        this.kategoriaId = kategoriaId;
+        this.nimi = nimi;
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
+
+    public Integer getKategoriaId() {
+        return kategoriaId;
+    }
+
+    public void setKategoriaId(Integer kategoriaId) {
+        this.kategoriaId = kategoriaId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 67 * hash + Objects.hashCode(this.kategoriaId);
+        hash = 67 * hash + Objects.hashCode(this.nimi);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Kategoria other = (Kategoria) obj;
+        if (!Objects.equals(this.nimi, other.nimi)) {
+            return false;
+        }
+        if (!Objects.equals(this.kategoriaId, other.kategoriaId)) {
+            return false;
+        }
+        return true;
+    }
+
+    
     
 }
