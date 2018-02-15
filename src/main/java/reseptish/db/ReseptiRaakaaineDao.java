c76fbba475a4cf65eab3671fc36d2cfb5e24f82b
mjaakko <jaakko.malkki@helsinki.fi>
2018-02-15 10:14:12 +0200

    reseptiraakaainedao

---
+++ src/main/java/reseptish/db/ReseptiRaakaaineDao.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reseptish.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import reseptish.pojo.ReseptiRaakaaine;
+
+/**
+ *
+ * @author jaakko
+ */
+public class ReseptiRaakaaineDao {
+    private Database db;
+
+    public ReseptiRaakaaineDao(Database db) {
+        this.db = db;
+    }
+    
+    public List<ReseptiRaakaaine> findAll() throws SQLException {
+        try (Connection c = db.getConnection()) {
+            PreparedStatement ps = c.prepareStatement("SELECT * FROM ReseptiRaakaine, Resepti, RaakaAine WHERE ReseptiRaakaine.resepti_id = Resepti.resepti_id AND Raakaaine.raakaaine_id = ReseptiRaakaaine.raakaaine_id");
+            
+            List<ReseptiRaakaaine> raakaaineet = new ArrayList<>();
+            
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                raakaaineet.add(ReseptiRaakaaine.rowToReseptiRaakaaine(rs));
+            }
+            
+            return raakaaineet;
+        }
+    }
+}
